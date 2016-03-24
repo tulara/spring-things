18c2ff76822bc31e40c3d9d6cd975f4bebd6b5ac
Tulara Webster <twebster@thoughtworks.com>
2016-03-24 11:17:14 -0600

    REST greeting: @ResponseBody with jackson message converter

---
+++ src/main/java/com/helloworld/model/Greeting.java
@@ -0,0 +1,22 @@
+package com.helloworld.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Greeting {
+    private String name = "World";
+    private String greeting = "Hello";
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s", this.greeting, this.name);
+    }
+}
--- src/main/java/com/helloworld/web/HelloRestController.java
+++ src/main/java/com/helloworld/web/HelloRestController.java
@@ -1,4 +1,21 @@
 package com.helloworld.web;
 
+import com.helloworld.model.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloRestController {
+
+    private Greeting greeting;
+
+    @Autowired
+    public HelloRestController(Greeting greeting){this.greeting = greeting;}
+
+    @RequestMapping(value="/greeting", method = RequestMethod.GET)
+    public Greeting greet() {
+        return greeting;
+    }
 }
