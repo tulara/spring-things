e5358ad9923ef40fb9b1bdbfd69dc986bd4db254
Tulara Webster <twebster@thoughtworks.com>
2016-03-29 11:13:54 -0600

    when books aren't found

--- src/main/java/example/bookstore/web/BookStoreController.java
+++ src/main/java/example/bookstore/web/BookStoreController.java
@@ -3,6 +3,8 @@ package example.bookstore.web;
 import example.bookstore.model.Book;
 import example.bookstore.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,13 +23,17 @@ public class BookStoreController {
     }
 
     @RequestMapping(value="/book/{id}", method= RequestMethod.GET)
-    public Book getBook(@PathVariable int id){
-        return bookService.getBookWithId(id);
+    public ResponseEntity<Book> getBook(@PathVariable int id){
+        Book book = bookService.getBookWithId(id);
+        return book == null ? new ResponseEntity<Book>(HttpStatus.NOT_FOUND) :
+                new ResponseEntity<Book>(book, HttpStatus.OK);
     }
 
-    @RequestMapping(value="/books", method= RequestMethod.GET)
-    public List<Book> getBooks(){
-        return bookService.getBooks();
+    @RequestMapping(value="/book", method= RequestMethod.GET)
+    public ResponseEntity<List<Book>> getBooks(){
+        List<Book> books = bookService.getBooks();
+        return books.isEmpty() ? new ResponseEntity<List<Book>>(HttpStatus.NOT_FOUND) :
+                new ResponseEntity<>(books, HttpStatus.OK);
     }
 
 
--- src/test/java/example/bookstore/web/BookStoreControllerTest.java
+++ src/test/java/example/bookstore/web/BookStoreControllerTest.java
@@ -1,30 +1,64 @@
 package example.bookstore.web;
 
+import example.bookstore.model.Book;
 import example.bookstore.service.BookService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.is;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 public class BookStoreControllerTest {
     //integration test
 
     MockMvc mockMvc;
+
+    @Mock
     BookService bookService = new BookService();
+
+    private static List<Book> sampleServiceResponse = Arrays.asList(
+            new Book("Jane Eyre", "Charlotte Brontë", 1),
+            new Book("Great Expectations", "Charles Dickens", 2));
 
     @Before
     public void setup() {
+        MockitoAnnotations.initMocks(this);
         mockMvc = standaloneSetup(new BookStoreController(bookService)).build();
     }
 
     @Test
     public void shouldHappilyReturnBookWithMatchingId() throws Exception {
+        Mockito.when(bookService.getBookWithId(1)).thenReturn(new Book("Jane Eyre", "Charlotte Brontë", 1));
         mockMvc.perform(get("/book/1"))
                 .andExpect(jsonPath("$.title", is("Jane Eyre")))
                 .andExpect(jsonPath("$.author", is("Charlotte Brontë")));
     }
+
+    @Test
+    public void bookWithNoMatchingIdShouldReturnNotFound() throws Exception {
+        Mockito.when(bookService.getBookWithId(-1)).thenReturn(null);
+        mockMvc.perform(get("/book/-1"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void shouldShowAllBooks() throws Exception {
+        Mockito.when(bookService.getBooks()).thenReturn(sampleServiceResponse);
+        mockMvc.perform(get("/book"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)));
+    }
+
+
 }
